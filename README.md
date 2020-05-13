431320c19507d13e700bc883685f10ac6f021b65
Gautam Mekkat <Blue9@users.noreply.github.com>
2020-05-12 22:09:13 -0400

    Create binary

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # os
 Simple operating system in Rust
+
+# Setup
+
+Run
+
+    rustup target add thumbv7em-none-eabihf
+
+# Build
+
+Run
+
+    cargo build --target thumbv7em-none-eabihf
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]  // We can't rely on the standard library
+#![no_main] // Disable entry point
+
+use core::panic::PanicInfo;
+
+/// Required for no_std applications
+/// See https://doc.rust-lang.org/nomicon/panic-handler.html
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+/// #[no_mangle] prevents function renaming so we can link to it.
+/// extern "C" means we use the C calling convention.
+///
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
 }
