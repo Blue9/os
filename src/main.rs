eea09512b489381bf70558ccb181e512b424f55f
Gautam Mekkat <Blue9@users.noreply.github.com>
2020-05-15 23:32:03 -0400

    Use VGA text mode for output

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,23 @@
-#![no_std]  // We can't rely on the standard library
+#![no_std] // We can't rely on the standard library
 #![no_main] // Disable entry point
 
 use core::panic::PanicInfo;
 
+mod vga_buffer;
+
 /// Required for no_std applications
 /// See https://doc.rust-lang.org/nomicon/panic-handler.html
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
-
 /// #[no_mangle] prevents function renaming so we can link to it.
 /// extern "C" means we use the C calling convention.
-///
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
+    println!("Hello {}{}", "world", "!");
+    // panic!("Some error");
     loop {}
 }
